19a24720d2a702833efb554fee0931f2aa10c188
unknown <j.soukchamroeun@gmail.com>
2020-04-02 09:43:07 -0700

    Configuration Updated

--- create_tables_mysql.py
+++ create_tables_mysql.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-db_conn = mysql.connector.connect(host="localhost", user="root",
+db_conn = mysql.connector.connect(host="ec2-54-203-161-163.us-west-2.compute.amazonaws.com", user="user",
                                   password="password", database="events")
 
 db_cursor = db_conn.cursor()
