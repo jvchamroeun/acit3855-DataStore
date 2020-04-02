97a71007ff0f139cfcd64eba88d2b7848f5e644f
unknown <j.soukchamroeun@gmail.com>
2020-04-02 14:02:59 -0700

    Configurations

--- drop_tables_mysql.py
+++ drop_tables_mysql.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-db_conn = mysql.connector.connect(host="ec2-52-43-100-165.us-west-2.compute.amazonaws.com", user="root",
+db_conn = mysql.connector.connect(host="ec2-54-188-73-8.us-west-2.compute.amazonaws.com", user="root",
                                   password="password", database="events")
 
 db_cursor = db_conn.cursor()
